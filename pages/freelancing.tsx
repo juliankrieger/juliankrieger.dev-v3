2ddd75e155c452ae29cd9f5b372280b6e5270602
juliankrieger <julian.krieger@me.com>
2021-06-29 01:05:58 +0200

    add gallery

---
+++ pages/freelancing.tsx
@@ -0,0 +1,33 @@
+import styles from './index.module.scss';
+import React from 'react';
+import Image from 'next/image';
+import DigiPIL from '../assets/images/DigiPIL.png';
+import style from './freelancing.module.scss';
+import { GalleryItem } from '../components/GalleryItem';
+
+export const Freelancing: React.FC = () => {
+    return (
+        <div>
+            <h3>Freelancing</h3>
+            <p>I do freelancing on the side when I get the chance. When I have the choice, this happens mostly
+                on the <a href={"https://www.mongodb.com/mern-stack"}>MERN</a> Stack, although I like switching up
+                Express with alternative server solutions. If a customer wishes for static rendering and small bundles,
+                I reach for <a href={"https://nextjs.org/"}>Next.js</a>. To make deployment and delivery easy, I bundle all my apps inside 
+                <a href={"https://www.docker.com/"}>Docker</a>, but deploying on serverless platforms is also possible.
+            </p>
+
+            <h4>Gallery</h4>
+            <p>A gallery of some of my recent and/or noteworthy freelancing projects</p>
+            <div className={style.GalleryGrid}>
+                <GalleryItem 
+                title={"DigiPIL"} 
+                image={{ src: DigiPIL, alt: "DigiPIL Platform" }} 
+                description="Online Platform for seraching and managing Patient Information Leaflets (PIL). Customers can register or login and save a list of their current medications along with a list of conditions. Shows possible interactions and side effects." 
+                tags={["Proof of Concept", "Persistent Database", "Database Admin Interface", "Session Cookie Authentification", "Docker Deployment", "IBM Carbon"]} />
+                                
+            </div>
+        </div>
+    )
+}
+
+export default Freelancing;
