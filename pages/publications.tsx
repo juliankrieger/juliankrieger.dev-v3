132f511628ed6b475e44d990e8d151d6e8a001fc
juliankrieger <julian.krieger@me.com>
2021-06-29 16:19:05 +0200

    update thesis

--- pages/publications.tsx
+++ pages/publications.tsx
@@ -18,7 +18,7 @@ export const Publications: React.FC = () => {
             <div className={style.GalleryGrid}>
                 <PublicationItem
                 title={isGerman ? "Über den Zusammenhang von Ladezeiten und Absprungraten": "About the correlation between website loading speeds and user bounce rates"} 
-                description="Language: German - Paper exploring the correlation between website loading speeds and user bounce rates " 
+                description="Language: German - Paper exploring the correlation between how long it takes a website to load its content and the eagerness of users to cancel loading and subsequently leaving the site early." 
                 date="15 July, 2019"
                 url={"/zusammenhang_ladenzeiten_absprungraten.pdf"}
                 /> 
@@ -27,6 +27,12 @@ export const Publications: React.FC = () => {
                 description="Language: German - Paper about state of the art techniques used to build and harden secure IoT home networks via StrongSwan"
                 date="30 August, 2019"
                 url={"/secure_homenetwork.pdf"}
+                /> 
+                <PublicationItem
+                title={isGerman ? "Entwicklung einer Internationalisierungslösung für das Content-Management-System Scrivito inklusive Evaluierung des Systems und Anwendung an einer internationalen Webseite.": "Development of an internationalization solution for the content management system Scrivito including evaluation of the system and application on an international website."} 
+                description="Language: German - Bachelor Thesis - Evaluation and comparison of a content management system regarding different popular industry solutions that was of interest for my employer at the time. The thesis' second part focused on a proof-of-concept of an internationalisation plugin since Scrivito itself was not equipped with a sufficient translation API at the time of writing."
+                date="30 August, 2019"
+                url={"/bachelor_thesis.pdf"}
                 /> 
             </div>
         </div>
