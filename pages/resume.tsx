7a24e8bc78139414f2c07470897d935e778bd050
juliankrieger <julian.krieger@me.com>
2022-01-24 17:01:16 +0100

    remove resume mdx

---
+++ pages/resume.tsx
@@ -0,0 +1,12 @@
+export default () => (
+    <div>
+        <h2>
+            Resume
+        </h2>
+        <p>
+            You can find the latest version of my resume here:
+            - <a href="/resume-en.pdf" download>English</a>
+            - <a href="/resume-de.pdf" download>German</a>
+        </p>
+    </div>
+)
