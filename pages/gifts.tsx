3e41e5ac674b1422356822863739b87a3a831de7
juliankrieger <julian.krieger@me.com>
2022-01-25 18:09:42 +0100

    add books

--- pages/gifts.tsx
+++ pages/gifts.tsx
@@ -1,33 +1,54 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export interface GiftProps {
-    data: string;
+interface OpenLibraryResponse {
+    reading_log_entries: [
+        {
+            work: {
+                title: string;
+                author_names: string[];
+                first_publish_year: number;
+                cover_edition_key: string;
+            }
+        }
+    ]
 }
 
-export const Gifts: React.FC<GiftProps> = ({data}) => {
+const OPEN_LIBRARY_URL = "https://openlibrary.org/people/juliankrieger/books/want-to-read.json";
+
+export const Gifts: React.FC= () => {
+
+    const [data, setData] = useState<OpenLibraryResponse | null>(null)
+
+    useEffect(() => {
+        fetch(OPEN_LIBRARY_URL)
+        .then((res) => res.json())
+        .then((data) => {
+            setData(data)
+        })
+    }, [])
 
     return (
         <div>
             <h2>Gifts</h2>
             <p>
-                If you are looking to gift me something (for whatever reason), you can find my Amazon wish list <Link href={data}>here</Link>
+                If you are looking to gift me something (for whatever reason), you can find <Link href={"https://www.amazon.de/hz/wishlist/ls/DI5WT15AHPLM?ref_=wl_share"}>my Amazon wish list here</Link>
             </p>
             <br/>
             <p>
                 Note: I am currently building a small webservice to display my wanted items here. Amazon has sadly shut down its wishlist API.
             </p>
+            <h3>Books</h3>
+            <p>Thanks to <b>Open Library</b>, you can find a list of books I want to read below</p>
+            <ul>
+            {
+                data?.reading_log_entries.map(entry => (
+                    <li>- {entry.work.title} by {entry.work.author_names[0]} ({entry.work.first_publish_year})</li>
+                ))
+            }
+            </ul>
         </div>
     )
 }
 
 export default Gifts;
-
-// This gets called on every request
-export async function getStaticProps() {
-
-    const data = "https://www.amazon.de/hz/wishlist/ls/DI5WT15AHPLM?ref_=wl_share";
-  
-    // Pass data to the page via props
-    return { props: { data }};
-}
