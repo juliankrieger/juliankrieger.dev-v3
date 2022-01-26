362d8d26ed0663fdb25fff22c4d52660a0e0cc56
juliankrieger <julian.krieger@me.com>
2022-01-26 09:20:29 +0100

    update resume page

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -6,9 +6,15 @@ const Resume = () => (
             Resume
         </h2>
         <p>
-            You can find the latest version of my resume here:
-            - <a href="/resume-en.pdf" download>English</a>
-            - <a href="/resume-de.pdf" download>German</a>
+            You can find the latest version of my education and employment history here:
+            <ul>
+                <li>
+                    - <a href="/resume-de.pdf" download>German Resume</a>
+                </li>
+                <li>
+                    - <a href="/resume-en.pdf" download>English Resume</a>
+                </li>   
+            </ul>
         </p>
     </div>
 )
