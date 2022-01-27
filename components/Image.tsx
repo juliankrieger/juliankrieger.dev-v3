212b4fe2817e84bedf42e078182dce6b4c20797c
juliankrieger <julian.krieger@me.com>
2022-01-27 23:41:07 +0100

    adjust images

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,39 +1,46 @@
 import React, { CSSProperties, ImgHTMLAttributes } from 'react';
 import styles from './Image.module.scss';
 import classNames from 'classnames';
-import NextImage, {ImageProps as NextImageProps} from 'next/image';
+import NextImage, { ImageProps as NextImageProps } from 'next/image';
 
-type ImageProps = NextImageProps & {style?: CSSProperties};
+type ImageProps = NextImageProps & { style?: CSSProperties };
 
-export const Image = ({style, className, ...restProps}: ImageProps) => {
-    return <div style={style}><NextImage {...restProps} loading='lazy' className={classNames([styles.Image, className])}/></div>
+export const Image = ({ style, className, ...restProps }: ImageProps) => {
+  return (
+  <div style={style}>
+    <NextImage 
+      loading='lazy' 
+      {...restProps}
+      className={classNames([styles.Image, className])}
+      />
+    </div>);
 }
 
 function generateStyleFromMarkdownOptions(options?: string): [string, CSSProperties] {
 
-  
+
   let alt = '';
   const styleObj: any = {};
 
-    if(options) {
+  if (options) {
 
-      const [altString, imageOptions] = options.split('[');
-      alt = altString;
+    const [altString, imageOptions] = options.split('[');
+    alt = altString;
 
-      const end = imageOptions.charAt(imageOptions.length - 1);
-      if(end !== ']') {
-        throw new Error(`Malformed Markdown Image Options - End character of options '${imageOptions}' should be ']', is: ${end}`)
-      }
-
-      const trimmedOptions = imageOptions.substring(0, imageOptions.length - 1);
-    
-      const optionElements = trimmedOptions.split(',');
-      for(const opt of optionElements) { 
-        const [lhs, rhs] = opt.split('=');
-        styleObj[lhs] = rhs;
-      }
-
+    const end = imageOptions.charAt(imageOptions.length - 1);
+    if (end !== ']') {
+      throw new Error(`Malformed Markdown Image Options - End character of options '${imageOptions}' should be ']', is: ${end}`)
     }
+
+    const trimmedOptions = imageOptions.substring(0, imageOptions.length - 1);
+
+    const optionElements = trimmedOptions.split(',');
+    for (const opt of optionElements) {
+      const [lhs, rhs] = opt.split('=');
+      styleObj[lhs] = rhs;
+    }
+
+  }
   return [alt, styleObj];
 }
 
@@ -44,12 +51,12 @@ export interface MarkdownImageProps {
 
 export const MarkdownImage = (props: MarkdownImageProps) => {
 
-  const {src, options} = props;
+  const { src, options } = props;
 
   const [alt, styleOptions] = generateStyleFromMarkdownOptions(options);
-  
+
   return (
     <span className={styles.MarkdownImageContainer}>
-      <Image className={styles.MarkdownImage} alt={alt} style={styleOptions} src={src}/>
+      <Image className={styles.MarkdownImage} alt={alt} style={styleOptions} src={src} />
     </span>);
 }
