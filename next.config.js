2403cdde170f1df2ad0cececa0c0ea4fe9f8e26d
juliankrieger <julian.krieger@me.com>
2022-01-28 00:09:35 +0100

    try to get styles to work again

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const withPlugins = require('next-compose-plugins');
-
 const nextJsConfig = {
   images: {
     deviceSizes: [640, 1024, 1600, 1920, 2048],
   },
 }
 
-module.exports = withPlugins(
-  [
-  ],
-  nextJsConfig
-)
+module.exports = nextJsConfig;
