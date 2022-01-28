edef22f225aa82ebc527d09ea7af12fc32900cd5
juliankrieger <julian.krieger@me.com>
2022-01-28 13:25:07 +0100

    add analyzing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
 const withPreact = require('next-plugin-preact');
+const withPlugins = require('next-compose-plugins');
 
+// empty plugin getting next config and returning it
+let withBundleAnalyzer = (args) => args;
 
-module.exports = withPreact({
-  reactStrictMode: true,
-  images: {
-    deviceSizes: [640, 1024, 1600, 1920, 2048],
+if (process.env.ANALYZE === 'true') {
+  withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+  })
+}
+
+module.exports = withPlugins(
+  [
+    [withBundleAnalyzer],
+    [withPreact]
+  ],
+  {
+    reactStrictMode: true,
+    images: {
+      deviceSizes: [640, 1024, 1600, 1920, 2048],
+    }
   }
-});
+);
