5e3a1aac9d6f268af7f2c76a969a2764df31b56d
juliankrieger <julian.krieger@me.com>
2022-01-28 12:58:38 +0100

    enable preact

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+const withPreact = require('next-plugin-preact');
+
+
+module.exports = withPreact({
   reactStrictMode: true,
   images: {
     deviceSizes: [640, 1024, 1600, 1920, 2048],
   }
-}
+});
